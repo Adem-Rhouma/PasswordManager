1e339c89e2789dc62e54aa298a0a39cf580a6337
Adem-Rhouma <adamstudioc24@gmail.com>
2025-09-14 07:01:07 +0100

    PL-12 Implement password hashing

--- src/main/java/Adam/Self/PasswordManager/controller/AuthController.java
+++ src/main/java/Adam/Self/PasswordManager/controller/AuthController.java
@@ -8,8 +8,10 @@ import Adam.Self.PasswordManager.model.User;
 import Adam.Self.PasswordManager.security.JwtUtil;
 import Adam.Self.PasswordManager.service.UserService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,9 +22,13 @@ public class AuthController {
     private final UserService userService;
     private final JwtUtil jwtUtil;
 
-    public AuthController(UserService userService,  JwtUtil jwtUtil) {
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(UserService userService,  JwtUtil jwtUtil, PasswordEncoder passwordEncoder) {
         this.userService = userService;
         this.jwtUtil = jwtUtil;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/register")
@@ -30,7 +36,7 @@ public class AuthController {
         User user = new User();
         user.setEmail(dto.getEmail());
         user.setName(dto.getName());
-        user.setPasswordHash(dto.getPassword());
+        user.setPasswordHash(passwordEncoder.encode(dto.getPassword()));
 
         try {
             userService.registerUser(user);
@@ -50,7 +56,7 @@ public class AuthController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid");
         }
 
-        if (!userLoginDtop.getPassword().equals(existingUser.getPasswordHash())){
+        if (!passwordEncoder.matches(userLoginDtop.getPassword(), existingUser.getPasswordHash())) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid");
         }
 
